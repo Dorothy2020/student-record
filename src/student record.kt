d8be5bd9aad8bc1f71fe8b42668edba66fc8c401
Dorothy2020 <wangarraakoth@gmail.com>
2021-02-20 23:19:50 +0300

    first commit

---
+++ src/student record.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val name = "Cynthia Jael"
+    println(name)
+    var age = 20
+    println(age)
+    val phoneNumber = "0722445625"
+    println(phoneNumber)
+    var weight = "40" + "kg"
+    println(weight)
+    val kenyan = true
+    println(kenyan)
+}
+
+
+
+
+
+
